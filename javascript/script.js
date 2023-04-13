cb6c85577f94528069dc2e55b617e12c9556f88c
AlbertoScalia <albscalia@gmail.com>
2023-04-13 10:00:09 +0200

    Correction

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
-const firstName = prompt("Inserisci il tuo nome");
+let firstName = prompt("Inserisci il tuo nome");
 
-const surname = prompt("Inserisci il tuo cognome");
+let surname = prompt("Inserisci il tuo cognome");
 
-const favoriteColor = prompt("Inserisci il tuo colore preferito");
+let favoriteColor = prompt("Inserisci il tuo colore preferito");
 
-const newPassword = firstName + surname + favoriteColor + 2023;
+let newPassword = `${firstName}-${surname}-${favoriteColor}-2023`;
 
 document.getElementById("password").innerHTML = newPassword;
